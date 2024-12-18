4618a1f693cc61f6ad01b9fc4b69d6776a249907
oslo-c <oslos_lot@proton.me>
2024-12-17 17:00:45 -0800

    Initial commit

---
+++ contracts/contracts/civitas-controller.sol
@@ -0,0 +1,6 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract CivitasController {
+    // TODO: Add the logic for the Civitas Controller
+}
---
+++ contracts/contracts/civitas-core.sol
@@ -0,0 +1,56 @@
+// SPDX-License-Identifier: MIT
+// Compatible with OpenZeppelin Contracts ^5.0.0
+pragma solidity ^0.8.22;
+
+import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import {ERC20Burnable} from "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
+import {ERC20Pausable} from "@openzeppelin/contracts/token/ERC20/extensions/ERC20Pausable.sol";
+import {ERC20Permit} from "@openzeppelin/contracts/token/ERC20/extensions/ERC20Permit.sol";
+import {ERC20Votes} from "@openzeppelin/contracts/token/ERC20/extensions/ERC20Votes.sol";
+import {Nonces} from "@openzeppelin/contracts/utils/Nonces.sol";
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
+
+contract CIVITAS is ERC20, ERC20Burnable, ERC20Pausable, Ownable, ERC20Permit, ERC20Votes {
+    uint256 public currentSupply;
+    
+    constructor(string memory _name, string memory _symbol)
+        Ownable(msg.sender)
+        ERC20(_name, _symbol)
+        ERC20Permit(_name)
+    {}
+
+    function pause() public onlyOwner {
+        _pause();
+    }
+
+    function unpause() public onlyOwner {
+        _unpause();
+    }
+
+    function mint(uint256 amount) public onlyOwner {
+        _mint(owner(), amount);
+    }
+
+    function burn(uint256 amount) public virtual override onlyOwner {
+        _burn(owner(), amount);
+    }
+
+    // The following functions are overrides required by Solidity.
+
+    function _update(address from, address to, uint256 value)
+        internal
+        override(ERC20, ERC20Pausable, ERC20Votes)
+    {
+        super._update(from, to, value);
+    }
+
+    function nonces(address owner)
+        public
+        view
+        override(ERC20Permit, Nonces)
+        returns (uint256)
+    {
+        return super.nonces(owner);
+    }
+}
+
